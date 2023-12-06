c54a483632acfa3b23f7c9afc6f62e1b71213bd0
ET0530 <ericthoo530@1utar.my>
2023-12-07 04:05:59 +0800

    add in login sign up features

--- app/src/main/java/my/edu/utar/petadoption/activities/MainActivity.java
+++ app/src/main/java/my/edu/utar/petadoption/activities/MainActivity.java
@@ -1,8 +1,13 @@
-package my.edu.utar.petadoption;
+package my.edu.utar.petadoption.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import my.edu.utar.petadoption.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn_chat = findViewById(R.id.imageButton3);
+
+
     }
 }
