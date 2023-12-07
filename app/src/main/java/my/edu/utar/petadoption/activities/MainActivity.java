b7aad106fb0a8303caee75bf91211bd059b64450
ET0530 <ericthoo530@1utar.my>
2023-12-07 18:56:13 +0800

    adding log out features

--- app/src/main/java/my/edu/utar/petadoption/activities/MainActivity.java
+++ app/src/main/java/my/edu/utar/petadoption/activities/MainActivity.java
@@ -3,7 +3,7 @@ package my.edu.utar.petadoption.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,6 +24,17 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnPos
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn_chat = findViewById(R.id.imageButton3);
+
+        btn_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChatSpace.class);
+                startActivity(intent);
+            }
+        });
+
 
         ImageButton postButton = findViewById(R.id.imageButton2);
 
@@ -55,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnPos
 
         if (resultCode == RESULT_OK && requestCode == USER_REQUEST_CODE) {
 
+        
             String title = data.getStringExtra("title");
             String description = data.getStringExtra("content");
             String imageUriString = data.getStringExtra("imageUri");
