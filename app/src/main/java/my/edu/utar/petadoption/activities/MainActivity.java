9ad0965c7a0556d41b0d1efef6beeb5d7ef41525
ET0530 <ericthoo530@1utar.my>
2023-12-07 21:25:23 +0800

    adding log out features

--- app/src/main/java/my/edu/utar/petadoption/activities/MainActivity.java
+++ app/src/main/java/my/edu/utar/petadoption/activities/MainActivity.java
@@ -14,11 +14,9 @@ import java.util.List;
 
 import my.edu.utar.petadoption.R;
 
-public class MainActivity extends AppCompatActivity implements PostAdapter.OnPostClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final int USER_REQUEST_CODE = 50;
-    private List<Post> posts = new ArrayList<>();
-    private PostAdapter postAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,68 +34,7 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnPos
         });
 
 
-        ImageButton postButton = findViewById(R.id.imageButton2);
-
-        postButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, WriteNewPost.class);
-            startActivityForResult(intent, USER_REQUEST_CODE);
-        });
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerViewPosts);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        postAdapter = new PostAdapter(posts, this);
-        recyclerView.setAdapter(postAdapter);
-
-        retrieveAndDisplayLocalPosts();
     }
-    private void retrieveAndDisplayLocalPosts() {
 
-        List<Post> localPosts = getLocalPosts();
-        posts.addAll(localPosts);
-        postAdapter.notifyDataSetChanged();
-    }
-    private List<Post> getLocalPosts() {
-        return new ArrayList<>();
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && requestCode == USER_REQUEST_CODE) {
-
-        
-            String title = data.getStringExtra("title");
-            String description = data.getStringExtra("content");
-            String imageUriString = data.getStringExtra("imageUri");
-            String birth = data.getStringExtra("birth");
-            String gender = data.getStringExtra("gender");
-            String characteristics = data.getStringExtra("characteristics");
-
-            Post newPost = new Post(title, description, Uri.parse(imageUriString), birth, gender, characteristics);
-
-            posts.add(0, newPost);
-            postAdapter.notifyDataSetChanged();
-        }
-    }
-    // Implementation of the OnPostClickListener interface method
-    @Override
-    public void onPostClick(int position) {
-        // Handle the click event for the selected post
-        Post selectedPost = posts.get(position);
-
-        // Create an Intent to navigate to the PostDetailActivity
-        Intent intent = new Intent(this, PostDetailActivity.class);
-
-        // Pass the selected post details to the PostDetailActivity
-        intent.putExtra("title", selectedPost.getTitle());
-        intent.putExtra("description", selectedPost.getContent());
-        intent.putExtra("imageUri", selectedPost.getImageUri().toString());
-        intent.putExtra("birth", selectedPost.getBirth());
-        intent.putExtra("gender", selectedPost.getGender());
-        intent.putExtra("characteristics", selectedPost.getCharacteristics());
-
-        // Start the PostDetailActivity
-        startActivity(intent);
-    }
 }
